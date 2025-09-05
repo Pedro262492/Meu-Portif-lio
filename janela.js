6d98df3c84076256bd0eaed4279698d77504b476
Pedro262492 <luanarodrigues9043@gmail.com>
2025-09-05 11:58:42 -0400

    Add files via upload

---
+++ janela.js
@@ -0,0 +1,190 @@
+
+
+
+const removido = document.getElementById("invisivel");
+
+ function abrirJanela (){
+
+  document.getElementById("musica").play();
+
+      const retangulo = document.getElementById("retangulo");
+      retangulo.classList.remove("invisivel");
+      retangulo.classList.add("visivel");
+
+
+      const barra = document.getElementById("barra");
+      barra.classList.remove("invisivel");
+      barra.classList.add("visivel");
+
+
+const fechar = document.getElementById("fechar");
+      fechar.classList.remove("invisivel");
+      fechar.classList.add("visivel");
+
+
+      let area1 = document.getElementById("area");
+      area1.classList.remove("invisivel");
+      area1.classList.add("visivel");
+
+      const area5 = document.getElementById("area5");
+      area5.classList.remove("visivel");
+      area5.classList.add("invisivel");
+
+      const area2 = document.getElementById("area2");
+      area2.classList.remove("visivel");
+      area2.classList.add("invisivel");
+
+     const area3 = document.getElementById("area3");
+      area3.classList.remove("visivel");
+      area3.classList.add("invisivel");
+
+      const area4 = document.getElementById("area4");
+      area4.classList.remove("visivel");
+      area4.classList.add("invisivel");
+    }
+
+    function fechar (){
+
+      document.getElementById("musica").play();
+
+       const retangulo = document.getElementById("retangulo");
+      retangulo.classList.remove("visivel");
+      retangulo.classList.add("invisivel");
+
+    
+      const barra = document.getElementById("barra");
+      barra.classList.remove("visivel");
+      barra.classList.add("invisivel");
+
+const fechar = document.getElementById("fechar");
+      fechar.classList.remove("visivel");
+      fechar.classList.add("invisivel");
+
+
+      let area = document.getElementById("area");
+      area.classList.remove("visivel");
+      area.classList.add("invisivel");
+
+      const area5 = document.getElementById("area5");
+      area5.classList.remove("visivel");
+      area5.classList.add("invisivel");
+
+      let area2 = document.getElementById("area");
+      area2.classList.remove("visivel");
+      area3.classList.add("invisivel");
+
+      let area3 = document.getElementById("area");
+      area3.classList.remove("visivel");
+      area3.classList.add("invisivel");
+
+      let area4 = document.getElementById("area");
+      area4.classList.remove("visivel");
+      area4.classList.add("invisivel");
+
+     const area1= document.getElementById("area");
+      area1.classList.remove("visivel");
+      area1.classList.add("invisivel");
+
+    }
+
+    function sobremim (){
+
+document.getElementById("musica").play();
+
+      const area = document.getElementById("area2");
+      area.classList.remove("invisivel");
+      area.classList.add("visivel");
+
+      const area4 = document.getElementById("area4");
+      area4.classList.remove("visivel");
+      area4.classList.add("invisivel");
+
+      const area3 = document.getElementById("area3");
+      area3.classList.remove("visivel");
+      area3.classList.add("invisivel");
+
+      const area5 = document.getElementById("area5");
+      area5.classList.remove("visivel");
+      area5.classList.add("invisivel");
+
+      const area1= document.getElementById("area");
+      area1.classList.remove("visivel");
+      area1.classList.add("invisivel");
+    }
+   
+    function programação (){
+
+      document.getElementById("musica").play();
+      
+      const area4 = document.getElementById("area4");
+      area4.classList.remove("invisivel");
+      area4.classList.add("visivel");
+
+      const area = document.getElementById("area2");
+      area.classList.remove("visivel");
+      area.classList.add("invisivel");
+
+      const area3 = document.getElementById("area3");
+      area3.classList.remove("visivel");
+      area3.classList.add("invisivel");
+
+      const area5 = document.getElementById("area5");
+      area5.classList.remove("visivel");
+      area5.classList.add("invisivel");
+
+      const area1= document.getElementById("area");
+      area1.classList.remove("visivel");
+      area1.classList.add("invisivel");
+    }
+
+    function projetos (){
+
+      document.getElementById("musica").play();
+      
+      const area3 = document.getElementById("area3");
+      area3.classList.remove("invisivel");
+      area3.classList.add("visivel");
+
+      const area = document.getElementById("area2");
+      area.classList.remove("visivel");
+      area.classList.add("invisivel");
+
+      const area4 = document.getElementById("area4");
+      area4.classList.remove("visivel");
+      area4.classList.add("invisivel");
+
+      const area5 = document.getElementById("area5");
+      area5.classList.remove("visivel");
+      area5.classList.add("invisivel");
+
+      const area1= document.getElementById("area");
+      area1.classList.remove("visivel");
+      area1.classList.add("invisivel");
+    }
+   
+    function contato (){
+
+      document.getElementById("musica").play();
+      
+      const area5 = document.getElementById("area5");
+      area5.classList.remove("invisivel");
+      area5.classList.add("visivel");
+
+      const area = document.getElementById("area2");
+      area.classList.remove("visivel");
+      area.classList.add("invisivel");
+
+      const area4 = document.getElementById("area4");
+      area4.classList.remove("visivel");
+      area4.classList.add("invisivel");
+
+      const area3= document.getElementById("area3");
+      area3.classList.remove("visivel");
+      area3.classList.add("invisivel");
+
+      const area1= document.getElementById("area");
+      area1.classList.remove("visivel");
+      area1.classList.add("invisivel");
+    }
+   
+    
